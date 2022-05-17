7d71d657b54e0220a348adb56d4f7ed28321ccf8
wangtengfei <776095293@qq.com>
2022-05-17 15:29:22 +0800

    修改变量错误

--- network/udp.go
+++ network/udp.go
@@ -18,7 +18,7 @@ func startUdp() error {
 	for {
 		// 读消息
 		var data = make([]byte, 1024)
-		len, tcpConn, err := listen.ReadFromUDP(data)
+		len, conn, err := listen.ReadFromUDP(data)
 		if err != nil {
 			log.Println(err)
 			continue
@@ -26,8 +26,8 @@ func startUdp() error {
 		log.Printf("接收消息,长度:%d, 数据:%s", len, string(data))
 
 		//发送消息
-		var writeData = tcpConn.AddrPort().Addr().String()
-		len, err = listen.WriteToUDP([]byte(writeData), tcpConn)
+		var writeData = conn.String()
+		len, err = listen.WriteToUDP([]byte(writeData), conn)
 		if err != nil {
 			log.Println(err)
 			continue
